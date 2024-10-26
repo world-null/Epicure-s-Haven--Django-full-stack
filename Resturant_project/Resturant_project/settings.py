48252132be28e651b07e818da379803bd9870e31
Shamba Chakraborty <world-null>
2024-10-27 03:40:57 +0530

    templates change

--- Resturant_project/Resturant_project/settings.py
+++ Resturant_project/Resturant_project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Resturant_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['TEMPLATE'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
