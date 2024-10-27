121d17648a9b44ded7f6afc8a149a6b60bd7647e
Shamba Chakraborty <world-null>
2024-10-27 13:42:30 +0530

    static change

--- Resturant_project/Resturant_project/settings.py
+++ Resturant_project/Resturant_project/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'Resturant_project', 'static')]
 
-MEDIA_URL = 'media/'
+MEDIA_URL = 'media/Items'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'Resturant_project', 'media')
 
 # Default primary key field type
