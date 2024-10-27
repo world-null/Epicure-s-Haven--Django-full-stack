bb2660e1d1cb177a3c29624d769c5ddac080895e
world-null <143930507+world-null@users.noreply.github.com>
2024-10-27 14:19:42 +0530

    Update settings.py

--- Resturant_project/Resturant_project/settings.py
+++ Resturant_project/Resturant_project/settings.py
@@ -117,11 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'Static')]
 STATIC_ROOT=os.path.join(BASE_DIR, 'assets')
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'Media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
