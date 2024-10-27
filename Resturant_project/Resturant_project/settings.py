ef1963a4b3029c3f6739cbd4d7a55da36cbff701
Shamba Chakraborty <world-null>
2024-10-27 14:33:20 +0530

    CSRF

--- Resturant_project/Resturant_project/settings.py
+++ Resturant_project/Resturant_project/settings.py
@@ -127,3 +127,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'Media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# settings.py
+CSRF_TRUSTED_ORIGINS = ['https://epicure-s-haven.onrender.com']
